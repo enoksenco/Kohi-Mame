d746abaccff05f5cd39ce4609a46f5244c682af4
Frida Elvestrand Enoksen <frida@enoksenco.no>
2025-11-15 21:07:55 +0100

    Added pages

---
+++ js/order.js
@@ -0,0 +1,10 @@
+/*For å få til overlayet har jeg brukt en guide fra https://www.w3schools.com/howto/howto_css_modals.asp? og https://dev.to/rajatamil/make-pop-up-modal-window-in-vanilla-javascript-4dd2? som inspirasjon*/
+
+const overlay = document
+  .getElementById("overlay")
+  .addEventListener("click", function () {
+    document.querySelector(".overlay-content").style.display = "block";
+  });
+document.getElementById("close-overlay").addEventListener("click", function () {
+  document.querySelector(".overlay-content").style.display = "none";
+});
